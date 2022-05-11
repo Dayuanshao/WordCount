5f13b5c5c84c9b586f646ffbf75d6aa3401cdbf1
Dayuanshao <ndys613@163.com>
2022-05-11 19:48:31 +0800

    第一次实验

---
+++ wordCount/wordcount.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <direct.h>
+
+void characterCount()
+{
+	FILE* file = fopen("myTest.txt", "r");
+	int charactercount = 0;//用于统计字符数
+	char c = ' '; //用于接收从文件里读取的字符
+	c = fgetc(file);
+	while (c != EOF) //EOF作为文件结束标志，这种以EOF作为文件结束标志的文件
+	{
+		charactercount++;
+		c = fgetc(file);
+	}
+	printf("统计出字符共有:%d", charactercount);
+	fclose(file);
+}
+
+void wordCount()
+{
+	FILE* file = fopen("myTest.txt", "r");
+	int wordcount = 0;//用于统计单词数目
+	char c = ' ';//用于接收从文件里读取的字符
+	int flag = 0;
+	c = fgetc(file);
+	printf("%c", c);
+	while (c != EOF)
+	{
+		if (c == ' ')
+		{
+			flag = 0;
+			c = fgetc(file);
+		}
+		else
+		{
+			if (flag == 0)
+			{
+				wordcount++;
+				flag = 1;
+			}
+			c = fgetc(file);
+		}
+	}
+	printf("统计出单词共有:%d", wordcount);
+	fclose(file);
+}
+int main(int argc, char* argv[])
+{
+	if ((strcmp(argv[1], "-c") == 0) && (strcmp(argv[2], "myTest.txt") == 0))
+		characterCount();
+	if ((strcmp(argv[1], "-w") == 0) && (strcmp(argv[2], "myTest.txt") == 0))
+		wordCount();
+	return 0;
+}
